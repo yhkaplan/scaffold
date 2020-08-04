1205a337e728e223947246ea353877a76140a160
yhkaplan <yhkaplan@gmail.com>
2020-08-04 19:23:59 +0900

    Update formula

--- Formula/scaffold.rb
+++ Formula/scaffold.rb
@@ -1,8 +1,8 @@
 class Scaffold < Formula
   desc "Tool for generating code from Stencil templates, similar to rails gen"
   homepage "https://github.com/yhkaplan/scaffold"
-  url "https://github.com/yhkaplan/scaffold/archive/0.1.0.tar.gz"
-  sha256 "8ed284359f5e93476d37c81f129bd251cd68b33cbd122f27b7d93ef2d61276f9"
+  url "https://github.com/yhkaplan/scaffold/archive/0.1.1.tar.gz"
+  sha256 "983f25eb9321cbc901394ee1d63fa1ad29b3117f8025df4c5b1c3a91b771938f"
   head "https://github.com/yhkaplan/scaffold.git"
 
   depends_on :xcode
