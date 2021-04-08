c171c4251b0a0cf3804f7e0fe9e9041d81aa7d1a
yhkaplan <yhkaplan@gmail.com>
2021-04-08 14:27:23 +0900

    Fix StencilSwiftKit filters not working and extra return characters appearing
    ref: https://github.com/yhkaplan/scaffold/issues/1

--- Sources/ScaffoldKit/TemplateRenderer.swift
+++ Sources/ScaffoldKit/TemplateRenderer.swift
@@ -4,11 +4,13 @@ import PathKit
 
 enum TemplateRenderer {
     static func renderTemplate(filePath: String, templateName: String, context: [String: Any]) throws -> String {
-        let path = Path(filePath)
-        let loader = FileSystemLoader(paths: [path])
-        let env = Environment(loader: loader)
-
         let fileName = templateName + ".stencil"
-        return try env.renderTemplate(name: fileName, context: context)
+        let path = Path(filePath) + Path(fileName)
+        let env = StencilSwiftKit.stencilSwiftEnvironment()
+        let templateString: String = try path.read()
+        let template = StencilSwiftTemplate(templateString: templateString, environment: env)
+
+        let enriched = try StencilContext.enrich(context: context, parameters: [:])
+        return try template.render(enriched)
     }
 }
